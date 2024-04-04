86099c7c50670ea444717e8b4e271da48c42660c
Daeonji <skaterboy198967@gmail.com>
2024-04-03 20:25:54 -0500

    fixed id field

--- elibray.js
+++ elibray.js
@@ -1,56 +1,64 @@
+const e = require("express");
 var express = require("express");
 
 var app = express();
 
-var bookList = new Map();
-bookList.set("1", {
-  title: "Reactions in React",
-  author: "Ben Dover",
-  publisher: "Random House",
-  isbn: "978-3-16-148410-0",
-  avail: "true",
-});
-bookList.set("2", {
-  title: "Express-sions",
-  author: "Frieda Livery",
-  publisher: "Chaotic House",
-  isbn: "978-3-16-148410-2",
-  avail: "true",
-});
-bookList.set("3", {
-  title: "Restful REST",
-  author: "Al Gorithm",
-  publisher: "ACM",
-  isbn: "978-3-16-143310-1",
-  avail: "true",
-});
-bookList.set("4", {
-  title: "See Essess",
-  author: "Anna Log",
-  publisher: "O'Reilly",
-  isbn: "987-6-54-148220-1",
-  avail: "false",
-  who: "Homer",
-  due: "1/1/23",
-});
-bookList.set("5", {
-  title: "Scripting in JS",
-  author: "Dee Gital",
-  publisher: "IEEE",
-  isbn: "987-6-54-321123-1",
-  avail: "false",
-  who: "Marge",
-  due: "1/2/23",
-});
-bookList.set("6", {
-  title: "Be An HTML Hero",
-  author: "Jen Neric",
-  publisher: "Coders-R-Us",
-  isbn: "987-6-54-321123-2",
-  avail: "false",
-  who: "Lisa",
-  due: "1/3/23",
-});
+let bookList = [
+  {
+    id: 1,
+    title: "Reactions in React",
+    author: "Ben Dover",
+    publisher: "Random House",
+    isbn: "978-3-16-148410-0",
+    avail: "true",
+  },
+  {
+    id: 2,
+    title: "Express-sions",
+    author: "Frieda Livery",
+    publisher: "Chaotic House",
+    isbn: "978-3-16-148410-2",
+    avail: "true",
+  },
+  {
+    id: 3,
+    title: "Restful REST",
+    author: "Al Gorithm",
+    publisher: "ACM",
+    isbn: "978-3-16-143310-1",
+    avail: "true",
+  },
+  {
+    id: 4,
+    title: "See Essess",
+    author: "Anna Log",
+    publisher: "O'Reilly",
+    isbn: "987-6-54-148220-1",
+    avail: "false",
+    who: "Homer",
+    due: "1/1/23",
+  },
+  {
+    id: 5,
+    title: "Scripting in JS",
+    author: "Dee Gital",
+    publisher: "IEEE",
+    isbn: "987-6-54-321123-1",
+    avail: "false",
+    who: "Marge",
+    due: "1/2/23",
+  },
+  {
+    id: 6,
+    title: "Be An HTML Hero",
+    author: "Jen Neric",
+    publisher: "Coders-R-Us",
+    isbn: "987-6-54-321123-2",
+    avail: "false",
+    who: "Lisa",
+    due: "1/3/23",
+  },
+];
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -69,15 +77,18 @@ app.use(function (req, res, next) {
 
 function getBooks() {
   const allBooks = [];
-  bookList.forEach((book, id) => {
-    allBooks.push({ id, book });
+  bookList.forEach((book) => {
+    allBooks.push(book);
   });
   return allBooks;
 }
 
 app.get("/books/:id", function (req, res) {
-  if (bookList.has(req.params.id)) {
-    res.json(bookList.get(req.params.id));
+  const bookId = parseInt(req.params.id);
+  const book = bookList.find((book) => book.id === bookId);
+
+  if (book) {
+    res.json(book);
     console.log(req.params.id);
   } else {
     res.sendStatus(404);
@@ -87,6 +98,21 @@ app.get("/books/:id", function (req, res) {
 app.get("/books", function (req, res) {
   const allBooks = getBooks();
   res.json(allBooks);
+});
+
+app.get("/books", function (req, res) {
+  const availQueryParam = req.query.avail;
+
+  if (availQueryParam === "true") {
+    const availBooks = bookList.filter((book) => book.avail === "true");
+    const formattedBooks = availBooks.map((book) => ({
+      id: book.id,
+      title: book.title,
+    }));
+    res.json(formattedBooks);
+  } else {
+    res.json([]); // Return empty array if avail parameter is not true
+  }
 });
 
 app.listen("3000");
